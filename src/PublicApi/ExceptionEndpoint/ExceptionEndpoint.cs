b5816ba85f4264582915281be5871827f9b22984
Ruslan Batkaev <ruslan_batkaev@epam.com>
2025-02-14 17:48:20 +0500

    Add exception throwing.
    Note: stacktrace is not visible in Application Insights.

---
+++ src/PublicApi/ExceptionEndpoint/ExceptionEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.eShopWeb.PublicApi.ExceptionEndpoint;
+
+public class ExceptionEndpoint : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        Get("api/exceptions");
+        AllowAnonymous();
+        Description(d => d.Produces<string>());
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        throw new Exception("Test Application Insights exception reporting");
+    }
+}
