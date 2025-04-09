a8ebab2643c2dabd193adea65bbde713788c44ef
Ruslan Batkaev <ruslan_batkaev@epam.com>
2025-04-09 14:43:38 +0500

    Add CosmosDB intergration

---
+++ src/Web/Features/OrderDeliveryPreparation/OrderDeliveryPreparation.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+
+namespace Microsoft.eShopWeb.Web.Features.OrderDeliveryPreparation;
+
+public class PrepareOrderForDelivery : IRequest
+{
+    public required Order Order { get; init; }
+}
